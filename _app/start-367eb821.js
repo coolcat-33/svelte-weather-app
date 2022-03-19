c2ef3aa8be33e09b10f6184a358d14174a5c91ce
coolcat-33 <coolcat-33>
2022-03-18 22:39:17 -0500

    Updates

--- _app/start-367eb821.js
+++ _app/start-367eb821.js
@@ -893,7 +893,7 @@ function notifiable_store(value) {
 }
 function create_updated_store() {
   const { set, subscribe } = writable(false);
-  const initial = "1647661080552";
+  const initial = "1647661152639";
   let timeout;
   async function check() {
     clearTimeout(timeout);
